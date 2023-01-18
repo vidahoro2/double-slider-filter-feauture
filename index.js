5233821f8e6f9e91529ea345a1914df2f20dbd7d
vidahoro2 <vidahoro@gmail.com>
2023-01-17 19:55:44 -0500

    version Victor

---
+++ index.js
@@ -0,0 +1,8 @@
+// const rangePrice = document.getElementById("range-price");
+// const minPrice = document.getElementById("min-price");
+// const maxPrice = document.getElementById("max-price");
+
+// rangePrice.addEventListener("input", function() {
+//   minPrice.innerHTML = this.value.split(",")[0];
+//   maxPrice.innerHTML = this.value.split(",")[1];
+// });
